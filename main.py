e280af05b0960387eaaeeeb31f5a739e18fd6e3b
Amelia Remnant <amelia.remnant@icloud.com>
2022-11-06 20:42:52 +0000

    company_name/summary

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-#hi
+from fastapi import FastAPI
+
+app = FastAPI()
+
+companies = {}
+
+@app.get("/{company_name}/summary}")
+def summary(company_name: str):
+    return companies[company_name]
+
+@app.post("/save/{company_name}/{review}")
+def save_company(company_name: str, review: str):
+    companies[company_name] = review
+    return companies[company_name]
+
+@app.delete("/delete/{company_name}")
+def remove_company(company_name: str):
+    del companies[company_name]
+
+
