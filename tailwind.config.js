119a31f0319accb1f104d75e6dcbe2c33a6260b7
Amelia Remnant <amelia.remnant@icloud.com>
2022-11-20 22:13:45 +0000

    Index 1.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  
+  //Looks for any html file in templates to compile
   content: ["./templates/*.html"],
   theme: {
-    extend: {},
+    
+    //setting breakpoints
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+
+    extend: {
+      
+      //Adding custom colours
+      colors: {
+        cherry: '#CD0F56',
+        iris: '#5D3fD3',
+        lilac: '#CF9FFF',
+        lightGrey: '#978C90',
+        lightRed: '#F98C9C',
+        gold: '#FFD700',
+        silver: '#C0C0C0',
+        bronze: '#CD7F32'
+      }
+
+    },
   },
   plugins: [],
 }
